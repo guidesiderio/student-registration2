218097d755420ae2a4499ffa57c8dc5ae62d4439
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-03 15:13:24 -0300

    Discipline information

--- app.py
+++ app.py
@@ -6,4 +6,14 @@ nome_aluno1 = aluno1[0]
 
 notas_aluno1 = aluno1[1]
 
-print(f'Nome: {nome_aluno1}, Notas: {notas_aluno1}')
+# Informações de uma disciplina
+
+alunos_lb = {}
+
+alunos_lb = {
+    'matricula1': ('aluno1', [10, 8, 7, 5]),
+    'matricula2': ('aluno2', [8, 8, 9, 6]),
+    'matricula3': ('aluno3', [9, 8, 6, 7])
+}
+
+
