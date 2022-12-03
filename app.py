f9206b831d252310f5e9d8d2464d0b69e512f4cc
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-03 15:08:48 -0300

    Student information

---
+++ app.py
@@ -0,0 +1,9 @@
+# Informações de um aluno
+
+aluno1 = ('Maria Joaquina', [10, 8, 9, 7])
+
+nome_aluno1 = aluno1[0]
+
+notas_aluno1 = aluno1[1]
+
+print(f'Nome: {nome_aluno1}, Notas: {notas_aluno1}')
